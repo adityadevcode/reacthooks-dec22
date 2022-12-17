2f15fb007596002179931ba049c1fa51cf23735f
adityadevcode <dev.adityacode@gmail.com>
2022-12-17 02:36:23 -0500

    not functioning code

--- src/Components/Useeffectupdated.js
+++ src/Components/Useeffectupdated.js
@@ -1,92 +1,191 @@
-
-//use effect does similiar thing to the class based component
-// componentdidmount, componentwillunmount, componentdidupdate lifecycle methods
-// Examples of such effects include logging, subscriptions, mutations, etc.
-// this hook uses an array of dependencies varables or states that useeffect AudioListenerto for changes
-// When their values change, the main body of the useEffect hook is executed
-// The return statement of this hook is used to clean methods that are already running, such as timers
-
-// useEffect(() => {
-//     some code
-// }, [myObject.property1, myObject.property2]);
-
-// Useeffect usecases
-// Running once on mount: fetch API data
-// Running on state change: validating input field
-// Running on state change: live filtering
-// Running on state change: trigger animation on new array value
-// Running on props change: update paragraph list on fetched API data update
-// Running on props change: updating fetched API data to get BTC updated price
-
-// When we want to perform an action once, especially when the app loads or mounts, we can use useEffect to do it.
-// we are triggering a fetch() GET request when the app is mounted, using an empty array as useEffect dependency.
+// USEEFFECT
 
 // import {useState, useEffect} from 'react';
 
 // const Useeffectupdated = () => {
-//     const[bio, setBio] =useState({});
+//     const[count,setCount] = useState(0)
 
-//     useEffect (() => {
-// const fetchData = async() => {
-//     const response = await fetch ('https://swapi.dev/api/people/1/');
-//     const data = await response.json();
-//     console.log(data);
-//     setBio(data);
-// };
-// fetchData();
-//     },[]);
-//     // -Empty dependencies array will make useEffect to run only once
-    
+//     useEffect (()=> {
+//         // console.log(count)
+//     })
+
 //   return (
-//   <>
-//     <hr/>
-//      <h3>UseEffect usecase</h3>
-//      <h3>Running once on mount:fetch api data</h3>
-//      <p>Aron skywalker bio:</p>
-//      {/* pre for preformatted text it will arrange text nicely */}
-//      <pre>{JSON.stringify(bio)}</pre>
-//      <pre>{JSON.stringify(bio)}</pre>
-   
-//      </>
+//     <>
+//     <h3>{count}</h3>
+//     </>
 //   )
 // }
 
 // export default Useeffectupdated;
 
-import {useEffect, useState} from "react";
+// when ever we want to perform action once, especially when the app loads or mounts
+// we use useeffect
+// import {useEffect, useState} from 'react';
 
-const Useeffectupdated = () => {
-const[input, setInput] = useState('');
-const[isValid, setIsValid]= useState(false);
+// const array = [
+//     { key: "1", type: "planet", value: "Tatooine" },
+//     { key: "2", type: "planet", value: "Alderaan" },
+//     { key: "3", type: "starship", value: "Death Star" },
+//     { key: "4", type: "starship", value: "CR90 corvette" },
+//     { key: "5", type: "starship", value: "Star Destroyer" },
+//     { key: "6", type: "person", value: "Luke Skywalker" },
+//     { key: "7", type: "person", value: "Darth Vader" },
+//     { key: "8", type: "person", value: "Leia Organa" },
+//   ];
+// //  const Useeffectupdated = (props) => {
+//   const Useeffectupdated = () => {
 
-// react prevent form submission when enter is pressed
-//   we need to create a form handler that would prevent the default form action
-const inputHandler = e => {
-    setInput(e.target.value);
-    console.log(inputHandler);
-};
+//   const [inputValue, setInputValue] = useState("");
+//   const [inputType, setInputType] = useState("");
+//   const [filteredArray, setFilteredArray] = useState(Array);
 
-useEffect(() => {
-    if (input.length < 5 || /\d/.test(input)) {
-                    setIsValid(false);
-                } else {
-                    setIsValid(true);
-                }
-            }, [input]);
+//   const inputValueHandler = (e) => {
+//         setInputValue(e.target.value);
+//         console.log(inputTypeHandler);
+//       };
+    
+//       const inputTypeHandler = (e) => {
+//         setInputType(e.target.value);
+//         console.log(inputTypeHandler);
+//       };
+    
+//       useEffect(() => {
+//         setFilteredArray((_) => {
+//           const newArray = array
+//             .filter((item) => item.value.includes(inputValue))
+//             .filter((item) => item.type.includes(inputType));
+//           return newArray;
+//         });
+//       }, [inputValue, inputType]);
+    
+//       // now prepare array to rendered
+    
+//       const listItems = filteredArray.map((item) => (
+//         <>
+//           <tr>
+//             <td style={{ border: "1px solid blue", padding: "0 1rem" }}>
+//               {item.type}
+//             </td>
+//             <td style={{ border: "1px solid red", padding: "0 1rem" }}>
+//               {item.value}
+//             </td>
+//           </tr>
+//         </>
+//       ));
 
- return (
-    <>
-     <h2>useEffect use case</h2>
-    <h3>Running on state change: validating input field</h3>
-            <form>
-                <label htmlFor="input">Write something (more than 5 non numerical characters is a valid input)</label><br />
-                <input type="text" id="input" autoComplete="off" onChange={inputHandler} style={{ height: '1.5rem', width: '20rem', marginTop: '1rem' }} />
-            </form>
-            <p><span style={isValid ? { backgroundColor: 'lightgreen', padding: '.5rem' } : { backgroundColor: 'lightpink', padding: '.5rem' }}>{isValid ? 'Valid input' : 'Input not valid'}</span></p>
-        </>
-    );
-//   }
-};
+//       return (
+//             <>
+//               <hr />
+//               <h2>useEffect use case</h2>
+//               <h3>Running on state change: live filtering</h3>
+//               <form
+//                 style={{
+//                   maxWidth: "23rem",
+//                   display: "flex",
+//                   justifyContent: "space-between",
+//                   alignItems: "center",
+//                 }}
+//               >
+//                 <div>
+//         {/*The htmlFor property sets or returns the value of the for attribute of a label.
+//         The for attribute specifies which form element a label is bound to.  */}
+//                   <label htmlFor="input-type">
+//                     Filter by <b>type</b>
+//                   </label>
+//                   <br />
+//                   <input
+//                     type="text"
+//                     id="input-type"
+//                     autoComplete="off"
+//                     onChange={inputTypeHandler}
+//                     style={{ height: "1.5rem", width: "10rem", marginTop: "1rem" }}
+//                   />
+//                 </div>
+//                 <div>
+//                   <label htmlFor="input-value">
+//                     Filter by <b>value</b>
+//                   </label>
+//                   <br />
+//                   <input
+//                     type="text"
+//                     id="input-value"
+//                     autoComplete="off"
+//                     onChange={inputValueHandler}
+//                     style={{ height: "1.5rem", width: "10rem", marginTop: "1rem" }}
+//                   />
+//                 </div>
+//               </form>
+//               <br />
+//               <table
+//                 style={{ width: "20rem", border: "2px solid purple", padding: "0 1rem" }}
+//               >
+//                 <tr>
+//                   <th>Type</th>
+//                   <th>Value</th>
+//                 </tr>
+//                 {listItems}
+//               </table>
+//             </>
+//           );
+//         };
+        
+// export default Useeffectupdated;
+
+//------------------------------------------
+//// As we are using a timer inside the useEffect, It is a good practice to clear it before
+// it gets set again by using the return statement of the useEffect,
+// which gets executed before the main body of the useEffect hook gets evaluated
+// (except for the first render).
 
 
-export default Useeffectupdated;
+// Running on props change update paragraph list on fetched API data update
+// we want to trigger a state update due to an updated fetch() call. We are sending the fetched data
+//  to a child component, and whenever that data is changed, the child component re-process it.
+
+// import {useEffect, useState} from "react";
+// import axios from "axios";
+
+
+// const Useeffectupdated = () => {
+//   const [userIDs, setUserIDs] = useState([]);
+//   const [user, setUser] = useState({});
+//   const [currentID, setCurrentID] = useState(1);
+
+//   const endPoint =
+//   "https://my-json-server.typicode.com/ifeanyidike/userdata/users";
+
+// useEffect(() => {
+//   axios.get(endPoint).then(({ data }) => setUserIDs(data));
+// }, []);
+
+// useEffect(() => {
+//   const fetchUserIDs = async () => {
+//     const { data } = await axios.get(`${endPoint}/${currentID}`);
+//     setUser(data);
+//   };
+
+//   fetchUserIDs();
+// }, [currentID]);
+
+// const moveToNextUser = () => {
+//   setCurrentID((prevId) => (prevId < userIDs.length ? prevId + 1 : prevId));
+// };
+// const moveToPrevUser = () => {
+//   setCurrentID((prevId) => (prevId === 1 ? prevId : prevId - 1));
+// };
+
+//   return (
+//     <div>
+//     <div>
+//       <h2>{user.name}</h2>
+//       <p>Occupation: {user.job}</p>
+//       <p>Sex: {user.sex}</p>
+//     </div>
+
+//   <button onClick={moveToPrevUser}>Prev</button>
+//   <button onClick={moveToNextUser}>Next</button>
+// </div>
+// );
+// }
+
+// export default Useeffectupdated;
